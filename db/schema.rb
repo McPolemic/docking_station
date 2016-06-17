192849619f749b002b66d79415160cd516e1fceb
Adam Lukens <alukens@purduefed.com>
2016-06-17 00:03:31 -0400

    Add a project URL to a project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160617025447) do
+ActiveRecord::Schema.define(version: 20160617031304) do
 
   create_table "builds", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160617025447) do
     t.string   "deploy_command"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "project_url"
   end
 
 end
