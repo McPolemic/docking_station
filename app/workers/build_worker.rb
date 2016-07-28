4962f82635bca03c511cdc9fa923ab95bf64f6ff
Adam Lukens <alukens@purduefed.com>
2016-07-28 10:23:14 -0400

    Add some build stuff

--- app/workers/build_worker.rb
+++ app/workers/build_worker.rb
@@ -7,21 +7,29 @@ class BuildWorker
     project = build.project
 
     Dir.mktmpdir do |temp_dir|
-      command = "git clone #{project.url} #{temp_dir} && cd #{temp_dir} && #{project.build_command_string(build.commit)}"
+      command = project.build_command_string(build.commit)
       logger.info "Running '#{command}'"
 
       build.build_output = "Building..."
       build.save
 
-      build.build_status, build.build_output = run_command( command )
+      build.build_status, _ = run_command( command ) { |line| build.build_output << line; build.save }
       build.save
     end
 
     TestWorker.perform_async(build_id)
   end
 
-  def run_command(command)
-    output = %x[ #{command} ]
+  def run_command(command, &block)
+    output = []
+
+    IO.popen(command) do |block|
+      block.each do |line|
+        yield line
+        output << line
+      end
+    end
+
     status = $? == 0
 
     [status, output]
