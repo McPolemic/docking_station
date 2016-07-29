66a8b26612c555c8e417176889394c11d1905e76
Adam Lukens <alukens@purduefed.com>
2016-07-29 00:57:26 -0400

    Add tests for Build#clear!

--- test/models/build_test.rb
+++ test/models/build_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class BuildTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "clearing clears build and test information" do
+    subject = Build.create(name: 'Test Build',
+                           build_output: "We're built",
+                           build_status: true,
+                           test_output: "And we're tested!",
+                           test_status: true)
+
+    subject.clear!
+
+    assert_equal(subject.build_output, "")
+    assert_equal(subject.build_status, false)
+    assert_equal(subject.test_output, "")
+    assert_equal(subject.test_status, false)
+  end
 end
